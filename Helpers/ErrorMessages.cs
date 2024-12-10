262074b5223f0a41067eaa87df7107e0886a4997
gbprg <duarte.gabrielgbd@gmail.com>
2024-12-10 02:04:26 -0300

    feat: remove baderrormessage

--- Helpers/ErrorMessages.cs
+++ Helpers/ErrorMessages.cs
@@ -3,7 +3,6 @@ namespace transfer_bank.Helpers
     public static class ErrorMessages
     {
         public const string GenericErrorMessage = "Ocorreu um erro inesperado. Tente novamente mais tarde.";
-        public const string BadRequestMessage = "Requisição inválida. Verifique os dados enviados.";
     }
 
 }
